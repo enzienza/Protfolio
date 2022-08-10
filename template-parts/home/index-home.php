46d7bb53b339798f9a463cff34c7c98606be692d
EnzaEnzi <enza.webdev@gmail.com>
2022-08-10 16:31:01 +0200

    :lipstick: add style in home section (:iphone: add responsive)

--- template-parts/home/index-home.php
+++ template-parts/home/index-home.php
@@ -9,7 +9,7 @@
  */
 ?>
 
-<div id="home" class="my-container" <?php if(checked(1, get_option('add_bg_hero'))) : ?>style="background-image: url(<?php echo get_option('bg_hero'); ?>)" <?php endif; ?>>
+<div id="home" class="hero-wrap" <?php if(checked(1, get_option('add_bg_hero'))) : ?>style="background-image: url(<?php echo get_option('bg_hero'); ?>)" <?php endif; ?>>
     <div class="jumb-content">
         <?php require_once('content/contents.php');?>
         <?php require_once('button/buttons.php');?>
